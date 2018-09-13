21673367f703bf0294121fdbd5dc3c3e235c2abc
Tobias Schürg <tobiasschuerg@gmail.com>
2018-09-13 17:00:57 +0200

    Added start script + reformatting.

--- backend.js
+++ backend.js
@@ -1,24 +1,25 @@
 const restify = require('restify')
 
-	const server = restify.createServer()
+const server = restify.createServer()
 
-	server.use(restify.plugins.queryParser())
-	server.use(restify.plugins.bodyParser())
+server.use(restify.plugins.queryParser())
+server.use(restify.plugins.bodyParser())
 
-	const messages = []
+const messages = []
 
-	server.get('/', (req, res) => {
-		return res.send(200, messages)
-	})
+server.get('/', (req, res) => {
+	return res.send(200, messages)
+})
 
-	server.post('/message', (req, res, next) => {
-		const body = req.body
-			body.timestamp = Date.now()
-			messages.push(req.body)
-			res.send(201)
-			return next()
-	})
+server.post('/message', (req, res, next) => {
+	const body = req.body
+	body.timestamp = Date.now()
+	messages.push(req.body)
+	res.send(201, body)
+	return next()
+})
 
-	server.listen(10000, () => {
-		console.log('%s listening at %s', server.name, server.url)
-	})
+const port = process.env.PORT || "0.0.0.0"
+server.listen(port, () => {
+	console.log('%s listening at %s', server.name, server.url)
+})
